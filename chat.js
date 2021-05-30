2aa31b11d760649fff3e4a1cdb717f14ba43dce4
Ong Zheng Lin <ongzhenglin1999@gmail.com>
2021-05-30 22:28:58 +0800

    Adapt build scripts to Heroku changes

--- chat.js
+++ chat.js
@@ -31,18 +31,27 @@ $(document).ready(function(){
 
 // Function to ask user to supply his/her name before entering a chatbox
 function loginMe() {
-     var person = prompt("Please enter your name:", "Sandip Salunke");//user;
+     var person = prompt("Please enter a username:", "");//user;
+      var name = person;
+      if (/([^\s])/.test(person) && person != null && person != "") {
 
-     if (/([^\s])/.test(person) && person != null && person != "") {
-         $('#user').val(person);
-         socket.emit('newUser', person);
-         document.title = person;
-     } else {
-         location.reload();
-     }
-    // $('#user').val(person);
-    //socket.emit('newUser', person);
-    // document.title = user;
+          $('#user').val(name);
+          socket.emit('newUser', name);
+          document.title = name;
+      } else {
+          location.reload();
+      }
+    // // $('#user').val(person);
+    // // socket.emit('newUser', person);
+    // // document.title = user;
+    //socket.emit('newUser', myUser.id)
+    // login();
+    // // var user = index.name;
+    // // var name = user.displayName;
+    // console.log(name);
+    // $('#user').val(name);
+    //        socket.emit('newUser', name);
+    //        document.title = name;
 }
 
 // Function to be called when sent a message from chatbox
